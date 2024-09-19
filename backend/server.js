970d74563902c73e8ba42682e8d2e9b267889810
Tushar-cm <79094147+Tushar-cm@users.noreply.github.com>
2024-09-19 12:26:00 +0530

    access key removed

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,7 @@ const mongoose=require("mongoose")
 const routesUrls=require("./routes/routes")
 const cors=require("cors")
 
-mongoose.connect("mongodb://atlas-sql-66eae5bd1c46023ac59a91d8-u6yp1.a.query.mongodb.net/sample_mflix?ssl=true&authSource=admin",()=>console.log("Database connected"))
-
+// mongoose.connect("add your mongodb atlas key here",()=>console.log("Database connected"))
 app.use(express.json())
 app.use(cors())
 app.use('/app',routesUrls)
